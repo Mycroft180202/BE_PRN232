b4b5151df49d0e873a7f20c08719c5cbf5de11e8
binhnguyen1709@gmail.com <binhnguyenn1709@gmail.com>
2025-06-25 08:48:35 +0700

    update

--- BE_PRN232/Configs/AppSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace BE_PRN232.Configs;
-
-public class AppSettings
-{
-    public string PathImage { get; set; }
-    public string BaseUrl { get; set; }
-    public string ClientUrl { get; set; }
-}
